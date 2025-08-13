760b0adb42a01bf4ad8ba3b5e97b214592d81efe
davydovalexander <schessmaster@gmail.com>
2025-08-12 20:37:10 -0500

    added functionality for training based on pendulum dynamics

--- dynamics.py
+++ dynamics.py
@@ -1,13 +1,13 @@
 import torch
 
-def f(x):
+def polynomial_f(x):
     # Polynomial vector field
     # TODO - make batched compatible
     return torch.tensor([-x[0] + x[2], 
     x[0]**2 - x[1] - 2*x[0]*x[2] + x[2], 
     -x[1]])
 
-def Df(x):
+def polynomial_Df(x):
     """
     Jacobian of polynomial vector field for batched input.
     
@@ -39,7 +39,7 @@ def Df(x):
     return J
 
 
-def jac_bounds(l, u):
+def polynomial_jac_bounds(l, u):
     # Interval bounds on Jacobian of polynomial dynamics given interval
     # bounds on the input
     # l, u: (b, 3) or (3,) if unbatched
@@ -75,6 +75,79 @@ def jac_bounds(l, u):
     
     return (lower, upper)
 
-def control_matrix():
+def polynomial_control_matrix():
     # Function for the constant control matrix B for the polynomial dynamics
     return torch.tensor([[0.], [0.], [1.]])
+
+def pendulum_f(x):
+    g = 10.
+    l = 1.
+    return torch.tensor([[x[1]], [g/l * torch.sin(x[0])]])
+
+def pendulum_Df(x):
+    """
+    Jacobian of pendulum vector field for batched input.
+    
+    Args:
+        x: Tensor of shape (b, 2) or (2,)
+    Returns:
+        Jacobian: Tensor of shape (b, 2, 2)
+    """
+    g = 10.
+    l = 1.
+    # Ensure batch dimension
+    if x.ndim == 1:
+        x = x.unsqueeze(0)
+
+    batch_size = x.shape[0]
+    J = torch.zeros(batch_size, 2, 2, dtype=x.dtype, device=x.device)
+
+    # Fill in the Jacobian
+    J[:, 0, 0] = 0.
+    J[:, 0, 1] = 1.
+
+    J[:, 1, 0] = -g/l * torch.cos(x[:, 0])
+    J[:, 1, 1] = 0
+
+    return J
+
+def pendulum_jac_bounds(l, u):
+    # Interval bounds on Jacobian of pendulum dynamics given interval
+    # bounds on the input
+    # l, u: (b, 2) or (2,) if unbatched
+    g = 10.
+    ell = 1.
+    # Ensure batching
+    if l.ndim == 1:
+        l = l.unsqueeze(0)
+        u = u.unsqueeze(0)
+    
+    b = l.shape[0]
+    device = l.device
+    dtype = l.dtype
+    
+    lower = torch.zeros((b, 2, 2), device=device, dtype=dtype)
+    upper = torch.zeros((b, 2, 2), device=device, dtype=dtype)
+    
+    # Constant entries
+    lower[:, 0, 0] = 0.
+    lower[:, 0, 1] = 1
+    lower[:, 1, 1] = -0.1
+    
+    upper[:, 0, 0] = 0
+    upper[:, 0, 1] = 1
+    upper[:, 1, 1] = -0.1
+    
+    # Fill in l/u dependent entries
+    lower[:, 1, 0] = -g/ell
+    upper[:, 1, 0] = g/ell*torch.maximum(torch.cos(l[:,0]), torch.cos(u[:,0]))
+    #lower[:, 1, 0] = -g/ell*torch.maximum(torch.cos(l[:,0]), torch.cos(u[:,0]))
+    #upper[:, 1, 0] = g/ell
+    
+    return (lower, upper)
+
+def pendulum_control_matrix():
+    # Function for the constant control matrix B for the pendulum dynamics
+    m = 1.
+    l = 1.
+    return torch.tensor([[0.], [1/(m*l**2)]])
